a2554fe0a49c498a61ce2a66a101df07ebd996b9
Prabesh Bhujel <prabeshbhujel02@gmail.com>
2025-11-30 22:14:29 +0545

    visualization

--- titanicDataset.py
+++ titanicDataset.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df=pd.read_csv(r'C:\LearnPy\practice\train.csv')
 
@@ -16,3 +18,20 @@ print(df["Fare"].max())
 #check unique values
 print(df["Sex"].unique())
 print(df["Embarked"].unique())
+
+#visualization
+#survival count
+# sns.countplot(data=df, x= 'Survived')
+# plt.show()
+
+#survival by gender
+# sns.countplot(data=df, x='Survived', hue ='Sex')
+# plt.show()
+
+#age Distribution
+sns.histplot(df['Age'].dropna(), bins=30)
+plt.show()
+
+#SurvivalByClass
+sns.countplot(data = df, x='Pclass', hue = 'Survived')
+plt.show()
